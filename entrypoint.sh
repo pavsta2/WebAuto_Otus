132dcee7fb7786409c5c7a5f8c30a3482c3b3e23
pavelstarshinov@gmail.com <pav$sta2>
2025-08-25 14:19:27 +0300

    homework Jenkins commit 59 no fetch

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@ set -e
 
 . .venv/bin/activate
 
+ALLURE_RESULTS="/root/WebAuto_Otus/allure-results"
+mkdir -p "$ALLURE_RESULTS"
+
+chmod -R 777 "$ALLURE_RESULTS"
+
 pytest tests/test_adm_login_page_elems.py --browser "$BROWSER" \
       --log_level "$LOG_LEVEL" \
       --browser_ver "$BROWSER_VER" \
@@ -10,5 +15,3 @@ pytest tests/test_adm_login_page_elems.py --browser "$BROWSER" \
       --remote_start \
       --remote_url="$REMOTE_URL" \
       --alluredir="$ALLURE_RESULTS"
-
-
