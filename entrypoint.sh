2a07fc53f8e53e08a555691a35cc9a93da573e83
pavelstarshinov@gmail.com <pav$sta2>
2025-08-26 20:53:04 +0300

    homework Jenkins commit success 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@ set +e
 
 . .venv/bin/activate
 
+ALLURE_RESULTS="${ALLURE_RESULTS:-/root/WebAuto_Otus/allure-results}"
+mkdir -p $ALLURE_RESULTS
+
+echo "=== Пишем результаты в: $ALLURE_RESULTS ==="
+
 pytest --browser "$BROWSER" \
       --log_level "$LOG_LEVEL" \
       --browser_ver "$BROWSER_VER" \
@@ -12,3 +17,9 @@ pytest --browser "$BROWSER" \
       --alluredir=$ALLURE_RESULTS
 
 chmod -R 777 $ALLURE_RESULTS
+
+echo "=== Содержимое $ALLURE_RESULTS ==="
+ls -la $ALLURE_RESULTS
+
+echo "=== Проверка первого JSON-файла ==="
+head -n 20 "$(find "$ALLURE_RESULTS" -name "*.json" -type f | head -1)"
